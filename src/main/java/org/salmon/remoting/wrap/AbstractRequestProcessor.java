536b036b9b2908f30a1064b0a1bddda211d4cbe0
wuwenyu <wuwenyu@wuwenyu-PC>
2015-10-08 18:30:23 +0800

    增加抽象实现 AbstractRequestProcessor

--- src/main/java/org/salmon/remoting/wrap/AbstractRequestProcessor.java
+++ src/main/java/org/salmon/remoting/wrap/AbstractRequestProcessor.java
@@ -1,20 +1,35 @@
 package org.salmon.remoting.wrap;
 
+import java.util.HashMap;
+
 import org.salmon.remoting.netty.NettyRequestProcessor;
 import org.salmon.remoting.protocol.RemotingCommand;
+import org.salmon.remoting.protocol.RemotingCommand.RemotingSysResponseCode;
 
 import io.netty.channel.ChannelHandlerContext;
 
-public class AbstractRequestProcessor implements NettyRequestProcessor {
+/**
+ * 抽象实现
+ * 
+ * @author wuwenyu
+ *
+ */
+public abstract class AbstractRequestProcessor implements NettyRequestProcessor {
 
 	@Override
 	public RemotingCommand processRequest(ChannelHandlerContext ctx, RemotingCommand request) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		RemotingCommand ret = RemotingCommand.createResponseCommand(RemotingSysResponseCode.SUCCESS, "success.");
+		ret.setBody(process(request.getExtFields(), request.getBody()));
+		return ret;
 	}
 
-	public byte[] process(byte[] request) {
-		return null;
-	}
+	/**
+	 * 插件需要实现处理方法
+	 * 
+	 * @param request
+	 * @return
+	 * @throws Exception
+	 */
+	public abstract byte[] process(HashMap<String, String> header, byte[] body) throws Exception;
 
 }
